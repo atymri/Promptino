9a1b77952665b9b124382a23b5a6d0a3672f2799
Amir Mahdi Temyouri <amirmahditeymoori123@gmail.com>
2025-11-30 15:54:23 +0330

    created mapping profiles

---
+++ Promptino.Core/Mappings/ImageProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Promptino.Core.Domain.Entities;
+using Promptino.Core.DTOs;
+
+namespace Promptino.Core.Mappings;
+
+public class ImageProfile : Profile
+{
+    public ImageProfile()
+    {
+        CreateMap<ImageAddRequest, Image>()
+            .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dst => dst.GeneratedWith, opt => opt.MapFrom(src => src.GeneratedWith))
+            .ForMember(dst => dst.Path, opt => opt.MapFrom(src => src.Path));
+
+        CreateMap<ImageUpdateRequest, Image>()
+            .ForMember(dst => dst.ID, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dst => dst.GeneratedWith, opt => opt.MapFrom(src => src.GeneratedWith))
+            .ForMember(dst => dst.Path, opt => opt.MapFrom(src => src.Path));
+
+        CreateMap<Image, ImageResponse>()
+            .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.ID))
+            .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dst => dst.GeneratedWith, opt => opt.MapFrom(src => src.GeneratedWith))
+            .ForMember(dst => dst.Path, opt => opt.MapFrom(src => src.Path));
+
+    }
+}
+
---
+++ Promptino.Core/Mappings/PromptProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Promptino.Core.Domain.Entities;
+using Promptino.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promptino.Core.Mappings;
+
+public class PromptProfile : Profile
+{
+    public PromptProfile()
+    {
+        CreateMap<PromptAddRequest, Prompt>()
+            .ForMember(dest => dest.ID, opt => opt.Ignore())
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Content, opt => opt.MapFrom(src => src.Content))
+            .ForMember(dest => dest.PromptImages, opt => opt.MapFrom(src => src.Images))
+            .ForMember(dest => dest.FavoritePrompts, opt => opt.Ignore());
+
+        CreateMap<PromptUpdateRequest, Prompt>()
+            .ForMember(dest => dest.ID, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Content, opt => opt.MapFrom(src => src.Content))
+            .ForMember(dest => dest.PromptImages, opt => opt.Ignore())
+            .ForMember(dest => dest.FavoritePrompts, opt => opt.Ignore());
+
+        CreateMap<Prompt, PromptResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ID))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Content, opt => opt.MapFrom(src => src.Content))
+            .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.PromptImages));
+    }
+}
